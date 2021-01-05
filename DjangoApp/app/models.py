e4684c553e043e64e0922793c6cc46a338b53341
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-05 18:21:53 +0100

    Añadidos modelos de pruebas

--- DjangoApp/app/models.py
+++ DjangoApp/app/models.py
@@ -4,17 +4,17 @@ from django.utils import timezone
 # Create your models here.
 
 class Libro(models.Model):
-    titulo = models.CharField(max_length=30)
-    autor  = models.CharField(max_length=30)
+    titulo = models.CharField(max_length = 30)
+    autor  = models.CharField(max_length = 30)
+    anio = models.IntegerField()
 
     def __str__(self):
         return "%s %s" % (self.titulo, self.autor)
 
-
 class Prestamo(models.Model):
-    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
-    fecha = models.DateField(default=timezone.now)
-    usuario = models.CharField(max_length=50)
+    libro = models.ForeignKey(Libro, on_delete = models.CASCADE)
+    fecha = models.DateField(default = timezone.now)
+    usuario = models.CharField(max_length = 50)
 
     def __str__(self):
         return "%s" % (self.usuario)
