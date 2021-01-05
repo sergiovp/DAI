39ae6a1440edcc2c5d121c64850303cad06334d5
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-05 18:22:10 +0100

    Añadidas las URLs para login y registro

--- DjangoApp/app/urls.py
+++ DjangoApp/app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('test_template', views.test_template, name='test_template'),
     path('libros', views.libros, name='libros'),
     path('prestamos', views.prestamos, name='prestamos'),
+    path('registro', views.registro, name='registro'),
+    path('login', views.login, name='login'),
 ]
