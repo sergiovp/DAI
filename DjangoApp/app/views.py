88f3f8944f313df21929de93e7de0c60766f5968
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-07 17:21:35 +0100

    Añadidas funciones para añadir y eliminar funciones

--- DjangoApp/app/views.py
+++ DjangoApp/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .forms import *
+from .models import *
 
 # Create your views here.
 
@@ -14,13 +15,70 @@ def index(request):
     return render(request, 'index.html')
 
 def libros(request):
-    return render(request, 'libros.html')
+    libritos = ''
+
+    libritos = Libros.objects.all()
+
+    return render(request, 'libros.html', {'libros': libritos})
+
+def add_libro(request):
+    libritos = ''
+    if (request.method == 'POST'):
+        form = add_libro_form(request.POST)
+
+        if (form.is_valid()):
+            form.save()
+            libritos = Libros.objects.all()
+            return render(request, 'libros.html', {'libros': libritos})
+
+    else:
+        form = add_libro_form()
+
+    return render(request, 'libros_form.html', {'form': form})
+    #return HttpResponse('Hello World!')
+
+def eliminar_libro(request, identificador):
+    libritos = ''
+    Libros.objects.filter(id = identificador).delete()
+
+    libritos = Libros.objects.all()
+
+    return render(request, 'libros.html', {'libros': libritos})
 
 def prestamos(request):
     return render(request, 'prestamos.html')
 
 def registro(request):
-    return render(request, 'registro.html')
+    if (request.method == 'POST'):
+        form = registro_form(request.POST)
+
+        if (form.is_valid()):
+            form.save()
+
+            return render(request, 'index.html')
+
+    else:
+        form = registro_form()
+
+    return render(request, 'registro.html', {'form': form})
 
 def login(request):
-    return render(request, 'login.html')
+    #return render(request, 'registro.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = login_form(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return 'HELLO'
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = login_form()
+
+    return render(request, 'login.html', {'form': form})
+
+    
