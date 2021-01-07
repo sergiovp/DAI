03e3a0365140e961ce888afa8b35affcdf5afea4
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-07 17:22:41 +0100

    Añadidos formularios para añadir libros

--- DjangoApp/app/forms.py
+++ DjangoApp/app/forms.py
@@ -1,1 +1,18 @@
 from django import forms
+from .models import *
+
+class registro_form(forms.ModelForm):
+    class Meta:
+        model = usuarios
+        password = forms.CharField(widget = forms.PasswordInput)
+        widgets = {'password': forms.PasswordInput()}
+        fields = ('usuario', 'password',)
+
+class login_form(forms.Form):
+    usuario = forms.CharField(label='Usuario', max_length=100)
+    password = forms.CharField(widget = forms.PasswordInput, label='Contraseña')
+
+class add_libro_form(forms.ModelForm):
+    class Meta:
+        model = Libros
+        fields = ('titulo', 'autor', 'anio',)
