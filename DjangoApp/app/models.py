09c591f02fa3d9d383dd9215b069f14c2334d146
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-07 17:22:09 +0100

    Pequeñas modificaciones en los modelos

--- DjangoApp/app/models.py
+++ DjangoApp/app/models.py
@@ -3,18 +3,25 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Libro(models.Model):
-    titulo = models.CharField(max_length = 30)
-    autor  = models.CharField(max_length = 30)
+class Libros(models.Model):
+    titulo = models.CharField(max_length = 50)
+    autor  = models.CharField(max_length = 50)
     anio = models.IntegerField()
 
     def __str__(self):
-        return "%s %s" % (self.titulo, self.autor)
+        return "%s %s %i" % (self.titulo, self.autor,self.anio)
 
-class Prestamo(models.Model):
-    libro = models.ForeignKey(Libro, on_delete = models.CASCADE)
+class prestamos(models.Model):
+    libro = models.ForeignKey(Libros, on_delete = models.CASCADE)
     fecha = models.DateField(default = timezone.now)
     usuario = models.CharField(max_length = 50)
 
     def __str__(self):
         return "%s" % (self.usuario)
+
+class usuarios(models.Model):
+    usuario = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return "%s %s" % (self.usuario, self.password)
