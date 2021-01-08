e5703efd6a0e730a77eeda739458958f62fc8ec9
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-08 17:48:50 +0100

    Añadido formulario para registro

--- DjangoApp/app/forms.py
+++ DjangoApp/app/forms.py
@@ -1,18 +1,27 @@
 from django import forms
 from .models import *
+from django.contrib.auth.models import User
 
 class registro_form(forms.ModelForm):
     class Meta:
-        model = usuarios
         password = forms.CharField(widget = forms.PasswordInput)
+        model = User
         widgets = {'password': forms.PasswordInput()}
-        fields = ('usuario', 'password',)
+        fields = ('username', 'password',)
+        help_texts = {
+            'username': None,
+        }
 
 class login_form(forms.Form):
     usuario = forms.CharField(label='Usuario', max_length=100)
     password = forms.CharField(widget = forms.PasswordInput, label='Contraseña')
 
 class add_libro_form(forms.ModelForm):
+    class Meta:
+        model = Libros
+        fields = ('titulo', 'autor', 'anio',)
+
+class set_libro_form(forms.ModelForm):
     class Meta:
         model = Libros
         fields = ('titulo', 'autor', 'anio',)
