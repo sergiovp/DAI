6fe65b45acfd50a894b6d619eb8a1b895aee615b
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-08 17:48:08 +0100

    Añadidas rutas para modificar y eliminar libros

--- DjangoApp/app/urls.py
+++ DjangoApp/app/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('add_libro', views.add_libro, name='add_libro'),
     path('eliminar_libro/<int:identificador>', views.eliminar_libro, name='eliminar_libro'),
+    path('modificar_libro/<int:identificador>', views.modificar_libro, name='modificar_libro'),
+    path('logout', views.logout, name='logout'),
+    path('reservar_libro/<int:identificador>', views.reservar_libro, name='reservar_libro'),
 ]
