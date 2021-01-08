29b6f9733040f905b92f44c4331dfa4fd4b089c1
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-08 17:48:31 +0100

    Añadidas algunas funciones controladoras

--- DjangoApp/app/views.py
+++ DjangoApp/app/views.py
@@ -6,20 +6,22 @@ from .models import *
 
 # Esta es la página controladora.
 
+usuario = ''
+
 # Página de ejemplo para saber que funciona
 def test_template(request):
     return HttpResponse('Hello World!')
 
 # Renderizamos la página index
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'usuario': usuario})
 
 def libros(request):
     libritos = ''
 
     libritos = Libros.objects.all()
 
-    return render(request, 'libros.html', {'libros': libritos})
+    return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
 
 def add_libro(request):
     libritos = ''
@@ -29,13 +31,60 @@ def add_libro(request):
         if (form.is_valid()):
             form.save()
             libritos = Libros.objects.all()
-            return render(request, 'libros.html', {'libros': libritos})
+            return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
 
     else:
         form = add_libro_form()
 
     return render(request, 'libros_form.html', {'form': form})
-    #return HttpResponse('Hello World!')
+
+def reservar_libro(request, identificador):
+    prestamo = Prestamos()
+    libritos = ''
+    libro = Libros()
+    global usuario
+
+    libro = Libros.objects.filter(id = identificador)
+
+    Libros.objects.filter(id = identificador).update(
+        reservado = True
+    )
+
+    prestamo.libro = Libros.objects.get(id = identificador)
+
+    prestamo.usuario = usuario
+
+    prestamo.save()
+    libritos = Libros.objects.all()
+
+    return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
+
+def modificar_libro(request, identificador):
+    libro = Libros.objects.filter(id = identificador)
+    print(libro)
+    if (request.method == 'POST'):
+        form = set_libro_form(request.POST)
+
+        if (form.is_valid()):
+            titulo = request.POST.get('titulo')
+            autor = request.POST.get('autor')
+            anio = request.POST.get('anio')
+
+            if (titulo and autor and anio):
+                Libros.objects.filter(id = identificador).update(
+                    titulo = titulo,
+                    autor = autor,
+                    anio = anio
+                )
+
+                libritos = Libros.objects.all()
+                print(titulo, autor, anio)
+                return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
+
+    else:
+        form = set_libro_form()
+
+    return render(request, 'modificar_libro.html', {'form': form, 'libro': libro})
 
 def eliminar_libro(request, identificador):
     libritos = ''
@@ -43,10 +92,11 @@ def eliminar_libro(request, identificador):
 
     libritos = Libros.objects.all()
 
-    return render(request, 'libros.html', {'libros': libritos})
+    return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
 
 def prestamos(request):
-    return render(request, 'prestamos.html')
+    prestamos = Prestamos.objects.all()
+    return render(request, 'prestamos.html', {'usuario': usuario, 'prestamos': prestamos})
 
 def registro(request):
     if (request.method == 'POST'):
@@ -63,6 +113,7 @@ def registro(request):
     return render(request, 'registro.html', {'form': form})
 
 def login(request):
+    global usuario
     #return render(request, 'registro.html')
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -70,15 +121,18 @@ def login(request):
         form = login_form(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return 'HELLO'
+            usuario = titulo = request.POST.get('usuario')
+            return render(request, 'index.html', {'usuario': usuario})
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = login_form()
 
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'login.html', {'form': form, 'usuario': usuario})
 
-    
+def logout(request):
+    global usuario
+
+    usuario = ''
+
+    return render(request, 'index.html', {'usuario': usuario})
