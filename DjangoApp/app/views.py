c4aa927c9d92848213fa12ff2bb315a930003d27
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-08 19:47:22 +0100

    Cambios en el código de acuerdo al sistema de gestión de sesiones

--- DjangoApp/app/views.py
+++ DjangoApp/app/views.py
@@ -8,30 +8,48 @@ from .models import *
 
 usuario = ''
 
+def check_session(request):
+    return 'usuario' in request.session
+
+def get_user_session(request):
+    return request.session['usuario']
+
 # Página de ejemplo para saber que funciona
 def test_template(request):
     return HttpResponse('Hello World!')
 
 # Renderizamos la página index
 def index(request):
+    global usuario
+
+    if (check_session(request)):
+        usuario = get_user_session(request)
+
     return render(request, 'index.html', {'usuario': usuario})
 
 def libros(request):
-    libritos = ''
+    global usuario
 
-    libritos = Libros.objects.all()
+    if (check_session(request)):
+        usuario = get_user_session(request)
 
-    return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
+    libros = Libros.objects.all()
+
+    return render(request, 'libros.html', {'libros': libros, 'usuario': usuario})
 
 def add_libro(request):
-    libritos = ''
+    global usuario
+
+    if (check_session(request)):
+        usuario = get_user_session(request)
+
     if (request.method == 'POST'):
         form = add_libro_form(request.POST)
 
         if (form.is_valid()):
             form.save()
-            libritos = Libros.objects.all()
-            return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
+            libros = Libros.objects.all()
+            return render(request, 'libros.html', {'libros': libros, 'usuario': usuario})
 
     else:
         form = add_libro_form()
@@ -39,10 +57,13 @@ def add_libro(request):
     return render(request, 'libros_form.html', {'form': form})
 
 def reservar_libro(request, identificador):
-    prestamo = Prestamos()
-    libritos = ''
-    libro = Libros()
     global usuario
+
+    if (check_session(request)):
+        usuario = get_user_session(request)
+
+    prestamo = Prestamos()
+    libro = Libros()
 
     libro = Libros.objects.filter(id = identificador)
 
@@ -54,14 +75,21 @@ def reservar_libro(request, identificador):
 
     prestamo.usuario = usuario
 
-    prestamo.save()
-    libritos = Libros.objects.all()
+    print(usuario)
 
-    return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
+    prestamo.save()
+    libros = Libros.objects.all()
+
+    return render(request, 'libros.html', {'libros': libros, 'usuario': usuario})
 
 def modificar_libro(request, identificador):
+    global usuario
+
+    if (check_session(request)):
+        usuario = get_user_session(request)
+
     libro = Libros.objects.filter(id = identificador)
-    print(libro)
+
     if (request.method == 'POST'):
         form = set_libro_form(request.POST)
 
@@ -77,9 +105,9 @@ def modificar_libro(request, identificador):
                     anio = anio
                 )
 
-                libritos = Libros.objects.all()
-                print(titulo, autor, anio)
-                return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
+                libros = Libros.objects.all()
+
+                return render(request, 'libros.html', {'libros': libros, 'usuario': usuario})
 
     else:
         form = set_libro_form()
@@ -87,52 +115,85 @@ def modificar_libro(request, identificador):
     return render(request, 'modificar_libro.html', {'form': form, 'libro': libro})
 
 def eliminar_libro(request, identificador):
-    libritos = ''
+    global usuario
+
+    if (check_session(request)):
+        usuario = get_user_session(request)
+
     Libros.objects.filter(id = identificador).delete()
 
-    libritos = Libros.objects.all()
+    libros = Libros.objects.all()
 
-    return render(request, 'libros.html', {'libros': libritos, 'usuario': usuario})
+    return render(request, 'libros.html', {'libros': libros, 'usuario': usuario})
 
 def prestamos(request):
+    global usuario
+
+    if (check_session(request)):
+        usuario = get_user_session(request)
+
     prestamos = Prestamos.objects.all()
+
     return render(request, 'prestamos.html', {'usuario': usuario, 'prestamos': prestamos})
 
 def registro(request):
+    error = ''
+
     if (request.method == 'POST'):
         form = registro_form(request.POST)
 
         if (form.is_valid()):
-            form.save()
+            user = Usuarios.objects.filter(usuario = request.POST.get('usuario'))
+            if (user):
+                error = 'Usuario en uso. Elige otro.'
+            else:
+                form.save()
+                # Establecer sesión:
+                request.session['usuario'] = request.POST.get('usuario')
+                request.session['password'] = request.POST.get('password')
 
-            return render(request, 'index.html')
+                return render(request, 'index.html', { 'usuario': request.session['usuario'] })
 
     else:
         form = registro_form()
 
-    return render(request, 'registro.html', {'form': form})
+    return render(request, 'registro.html', {'form': form, 'error': error})
 
 def login(request):
-    global usuario
-    #return render(request, 'registro.html')
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = login_form(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            usuario = titulo = request.POST.get('usuario')
-            return render(request, 'index.html', {'usuario': usuario})
+    error = ''
 
-    # if a GET (or any other method) we'll create a blank form
+    if (request.method == 'POST'):
+        form = login_form(request.POST)
+
+        if (form.is_valid()):
+
+            # Comprobamos BD
+            user = Usuarios.objects.filter(usuario = request.POST.get('usuario'))
+            passw = Usuarios.objects.filter(password = request.POST.get('password'))
+
+            # LogIn correcto
+            if (user and passw):
+
+                # Establecemos la sesión
+                request.session['usuario'] = request.POST.get('usuario')
+                request.session['password'] = request.POST.get('password')
+
+                return render(request, 'index.html', {'usuario': request.session['usuario']})
+            else:
+                error = 'Usuario o contraseña incorrectos.'
     else:
         form = login_form()
 
-    return render(request, 'login.html', {'form': form, 'usuario': usuario})
+    return render(request, 'login.html', { 'form': form, 'error': error })
 
+# Deslogueo
 def logout(request):
     global usuario
 
     usuario = ''
 
-    return render(request, 'index.html', {'usuario': usuario})
+    # Eliminamos la sesión
+    del request.session['usuario']
+    del request.session['password']
+
+    return render(request, 'index.html', {'usuario': ''})
