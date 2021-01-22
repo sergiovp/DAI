5bb13acbd4ced523b4a8db8879b7a137d73a496e
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-01-22 20:21:01 +0100

    Script para comprobar que el nombre de usuario no está cogido en tiempo real

---
+++ FlaskApp/app/static/js/validacion_registro.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#login').keyup(function() {
+        let letrasFormulario = $(this).val();
+        let usuarios = Array();
+
+        var usua = document.getElementsByClassName("usuarios");
+        for (let i = 0; i < usua.length; i++) {
+            usuarios.push(document.getElementsByClassName("usuarios")[i].innerHTML);
+        }
+
+        for (let i = 0; i < usuarios.length; i++) {
+            if (letrasFormulario == usuarios[i]) {
+                document.getElementById('mensaje_usuario').style.display = 'block';
+                break;
+            } else {
+                console.log("letras: ", letrasFormulario);
+                console.log("usuario:", usuarios[i]);
+                document.getElementById('mensaje_usuario').style.display = 'none';
+            }
+        }
+    });
+});
